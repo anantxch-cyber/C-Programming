69369d979072822a6366524e7aadf68bbc185911
ANANT KUMAR AGNIHOTRI <anantxch@ANANTs-MacBook-Air.local>
2026-05-08 19:00:15 +0530

    Added loops programs

---
+++ basics_variables_operators_input/float_Question2.c
@@ -0,0 +1,16 @@
+// PRINT PERCENTAGE OF 4 SUBJECTS WHOSE MARKS ARE OUT OF 40
+#include<stdio.h>
+int main(){
+    float sub1,sub2,sub3,sub4;
+    float total,percentage;
+
+    printf("Enter marks of 4 subjects (out of 40):\n");
+    scanf("%f %f %f %f", &sub1, &sub2, &sub3, &sub4);
+
+    total = sub1 + sub2 + sub3 + sub4;
+    percentage = (total / 160) * 100;
+
+    printf("Total marks obtained: %.2f\n",total);
+    printf("The percentage is : %.2f%%\n",percentage);
+    return 0;
+}
