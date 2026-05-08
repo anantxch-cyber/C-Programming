1bc00659324db00e2f66f54bbbc0f828466fc262
ANANT KUMAR AGNIHOTRI <anantxch@ANANTs-MacBook-Air.local>
2026-05-08 22:41:13 +0530

    Added loops programs

--- basics_variables_operators_input/area_of_circle.c
+++ basics_variables_operators_input/area_of_circle.c
@@ -1,8 +1,8 @@
 // WRITING CODE FOR AREA OF A CIRCLE WITH GIVEN RADIUS, FORMULA : PI * R * R
 #include<stdio.h>
 int main(){
-    int r = 18;
-    float Area = 3.14 * r * r;
+    int radius = 18;
+    float Area = 3.14 * radius * radius;
     printf("The area of circle is : %f",Area);
     return 0;
 }
