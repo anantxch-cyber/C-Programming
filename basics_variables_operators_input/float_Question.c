d290d1cd7b4c56453a3029acf55a42161140f2a8
ANANT KUMAR AGNIHOTRI <anantxch@ANANTs-MacBook-Air.local>
2026-05-08 18:39:40 +0530

    Added loops programs

---
+++ basics_variables_operators_input/float_Question.c
@@ -0,0 +1,12 @@
+// CALCULATING PERCENTAGE OF 5 SUBJECTS AND FINDING OUT THE GRADE
+#include<stdio.h>
+int main(){
+    float x1 = 85;
+    float x2 = 90;
+    float x3 = 78;
+    float x4 = 92;
+    float x5 = 88;
+    float percentage = (x1 + x2 + x3 + x4 + x5)/5;
+    printf("The percentage is : %.2f\n",percentage);
+    return 0;
+}
