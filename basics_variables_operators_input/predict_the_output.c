9c5e0c7a5bc44527de958a2022f553516a5981fe
ANANT KUMAR AGNIHOTRI <anantxch@ANANTs-MacBook-Air.local>
2026-05-09 01:10:59 +0530

    Added loops programs

---
+++ basics_variables_operators_input/predict_the_output.c
@@ -0,0 +1,13 @@
+// PROGRAMMING TO PREDICT THE OUTPUT
+#include<stdio.h>
+int main(){
+    
+    int p,q;
+    printf("Enter the value of p and q :\n");
+    scanf("%d %d",&p,&q); // & is for storing value into memory address
+    
+    printf("The value of p is : %d\n",p);
+    printf("The value of q is : %d\n",q);
+    
+    return 0;
+}
