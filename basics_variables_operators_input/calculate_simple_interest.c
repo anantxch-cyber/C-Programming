080342f2d8b77184d9030f4bd9b606671ab51406
ANANT KUMAR AGNIHOTRI <anantxch@ANANTs-MacBook-Air.local>
2026-05-08 23:00:42 +0530

    Added loops programs

--- basics_variables_operators_input/calculate_simple_interest.c
+++ basics_variables_operators_input/calculate_simple_interest.c
@@ -3,9 +3,9 @@
 int main(){
     float p,r,t,si;
     printf("Enter the principal amount, rate of interest and time in years :\n");
-    scanf("%f %f %f",&p,&r,&t);
+    scanf("%f %f %f",&p,&r,&t); // & is for storing value into memory address
 
-    si = (p * r * t) / 100;
+    si = (p * r * t) / 100; // SI = (P * R * T) / 100
     printf("The simple interest is : %.2f\n",si);
     return 0;
 }
