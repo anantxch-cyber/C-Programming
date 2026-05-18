d273732863f08b92e38bf568d2085e14d6ecc264
ANANT KUMAR AGNIHOTRI <anantxch@ANANTs-MacBook-Air.local>
2026-05-18 15:16:20 +0530

    Added loops programs

--- basics_variables_operators_input/hierarchy_of_operators.c
+++ basics_variables_operators_input/hierarchy_of_operators.c
@@ -1,4 +1,5 @@
-// // This program demonstrates operator precedence and integer division in C
+// This program demonstrates operator precedence and integer division in C
+// Float is only accurate till 6-7 decimal places, so if we want to get more accurate result then we can use double data type which is accurate till 15-16 decimal places. But in this program we are using int data type for all the variables, so the result will be an integer and not a float.
 #include<stdio.h>
 int main(){
     int i;
