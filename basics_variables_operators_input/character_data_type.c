a6ab735d1b10317ce164a5097a77766d6d92255f
ANANT KUMAR AGNIHOTRI <anantxch@ANANTs-MacBook-Air.local>
2026-05-18 15:28:30 +0530

    Added loops programs

---
+++ basics_variables_operators_input/character_data_type.c
@@ -0,0 +1,6 @@
+// This program demonstrates the use of character data type in C
+#include<stdio.h>
+int main(){
+    char ch = 'A'; // character data type can hold only a single character, so it will only store 'A' and not 'ANANT'
+    printf("The value of ch is : %c\n",ch); // %c is format specifier for character data type
+    return 0;}
